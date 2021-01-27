164ed88166834bd4c2cbd39bf3184a63c953f04a
Amanda Hernandez <amandah2@usf.edu>
2021-01-26 19:11:44 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿/*
+ Name: Amanda Hernandez
+Date:1/26/2021
+Description: ISM 4300 Deliverable 2 - Conditional Statements
+*/
+using System;
+
+namespace ConditionalStatements_ISM4300
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompt user to insert grade
+            Console.WriteLine("What is your expected grade for ISM 4300?");
+
+            try
+            {
+                //read input and assign variable grade as integer
+                string input = Console.ReadLine();
+                int grade = int.Parse(input);
+
+                //if else if statement to assign letter grades to numeric value
+
+                if (grade >= 90)
+                {
+                    Console.WriteLine("A");
+                    Console.WriteLine("Press any key to exit");
+                    Console.ReadKey(true);
+                }
+                else if (grade >= 80)
+                {
+                    Console.WriteLine("B");
+                    Console.WriteLine("Press any key to exit");
+                    Console.ReadKey(true);
+                }
+                else if (grade >=70)
+                {
+                    Console.WriteLine("C");
+                    Console.WriteLine("Press any key to exit");
+                    Console.ReadKey(true);
+                }
+
+                else if (grade >= 60)
+                {
+                    Console.WriteLine("D");
+                    Console.WriteLine("Press any key to exit");
+                    Console.ReadKey(true);
+                }
+
+                else
+                {
+                    Console.WriteLine("F");
+                    Console.WriteLine("Press any key to exit");
+                    Console.ReadKey(true);
+                }
+                
+
+            }
+            //try ends
+          
+            catch
+            {
+                //tell user to type integer when they put noninteger characters
+                Console.WriteLine("Please enter an integer value");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey(true);
+            }
+            //catch ends
+            }
+        }
+    }
+
